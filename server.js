f07931c6873504d8caab935bd4a9622d41bafac6
Alfian Rikzandi <denydirgantara94@gmail.com>
2020-08-17 13:27:39 +0700

    update port with host binding

--- server.js
+++ server.js
@@ -233,7 +233,7 @@ fastify.get('/', async (request, reply) => {
 // Run the server!
 const start = async () => {
     try {
-        await fastify.listen(PORT)
+        await fastify.listen(PORT, '0.0.0.0')
         fastify.log.info(`server listening on ${fastify.server.address().port}`)
     } catch (err) {
         fastify.log.error(err)
