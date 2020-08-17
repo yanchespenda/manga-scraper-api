04b121b9d30343a3ebfc517bc3f78618e6d9699e
Alfian Rikzandi <denydirgantara94@gmail.com>
2020-08-17 13:14:42 +0700

    update port

--- server.js
+++ server.js
@@ -231,8 +231,9 @@ fastify.get('/', async (request, reply) => {
 
 // Run the server!
 const start = async () => {
+    const port = process.env.PORT || 3000
     try {
-        await fastify.listen(3000)
+        await fastify.listen(port)
         fastify.log.info(`server listening on ${fastify.server.address().port}`)
     } catch (err) {
         fastify.log.error(err)
