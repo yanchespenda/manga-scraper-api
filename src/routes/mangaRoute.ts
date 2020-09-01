d4ced1ac0f63c0141b45f75e8b06ab6dc9eb507b
Alfian Rikzandi <denydirgantara94@gmail.com>
2020-09-01 11:59:37 +0700

    add base route & frontend routes

--- src/routes/mangaRoute.ts
+++ src/routes/mangaRoute.ts
@@ -1,4 +1,6 @@
 import * as MangaController from '../controllers/MangaController';
+import * as BaseController from '../controllers/BaseController'
+import * as FrontendController from '../controllers/FrontendController'
 import { RouteOptions } from 'fastify';
 
 const getMangaRoute: RouteOptions = {
@@ -19,12 +21,33 @@ const getProxyRoute: RouteOptions = {
 	handler: MangaController.getProxy,
 };
 
+const getIndex: RouteOptions = {
+	method: 'GET',
+	url: '/',
+	handler: BaseController.getIndex,
+};
+
+/* Frontend Routes */
+const getFESupport: RouteOptions = {
+	method: 'GET',
+	url: '/api/_next/support',
+	handler: FrontendController.getIndexSupportSite,
+};
+
 /* const createMangaRoute: RouteOptions = {
 	method: 'POST',
 	url: '/api/manga',
 	handler: MangaController.createManga,
 }; */
 
-const routes = [getMangaRoute, getDownloadRoute, getProxyRoute];
+const routes = [
+	getIndex,
+
+	getFESupport,
+	
+	getMangaRoute,
+	getDownloadRoute,
+	getProxyRoute
+];
 
 export default routes;
